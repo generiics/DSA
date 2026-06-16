102e7d31f21cb6c1e1b960e9e338efcb56c5a21a
Sudhir Kumar <sudhir.rit15@gmail.com>
2026-06-16 22:55:37 +0530

    Time: 4 ms (76.47%) | Memory: 55.5 MB (36.76%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch >= 'a' && ch <= 'z') {
+                sb.append(ch);
+            }
+
+            else if (ch == '*') {
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+
+            else if (ch == '#') {
+                sb.append(sb.toString()); // duplicate current result
+            }
+
+            else if (ch == '%') {
+                sb.reverse();
+            }
+        }
+
+        return sb.toString();
+    }
+}
