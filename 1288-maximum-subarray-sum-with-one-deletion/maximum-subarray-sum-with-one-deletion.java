10bf75761a128a509f0046685a4239f459847651
Sudhir Kumar <sudhir.rit15@gmail.com>
2026-07-03 22:06:41 +0530

    Time: 6 ms (91.30%) | Memory: 55.9 MB (48.47%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.java
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.java
@@ -1,23 +1,18 @@
 class Solution {
     public int maximumSum(int[] arr) {
-        int n=arr.length;
-        int[] left=new int[n];
-        int[] right=new int[n];
-
-        left[0]=arr[0];
         int ans=arr[0];
+        int delete=0;
+        int keep=arr[0];
 
-        for(int i=1;i<n;i++){
-            left[i]=Math.max(arr[i],left[i-1]+arr[i]);
-            ans=Math.max(ans,left[i]);
-        }
-        right[n-1]=arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            right[i]=Math.max(arr[i],right[i+1]+arr[i]);
-        }
-        for(int i=1;i<n-1;i++){
-            ans=Math.max(ans,left[i-1]+right[i+1]);
+        for(int i=1;i<arr.length;i++){
+            int prevkeep=keep;
+            keep=Math.max(keep+arr[i],arr[i]);
+            delete=Math.max(delete+arr[i],prevkeep);
+        
+        ans=Math.max(ans,Math.max(keep,delete));
         }
         return ans;
+
+        
     }
 }
