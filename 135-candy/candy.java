450982a7ac76274615e7b0b5d02a5fa2e4466810
Sudhir Kumar <sudhir.rit15@gmail.com>
2026-07-09 23:17:07 +0530

    Time: 2 ms (99.46%) | Memory: 47.8 MB (32.51%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -6,14 +6,16 @@ class Solution {
         for(int i=0;i<n;i++){
             ans[i]=1;
         }
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                ans[i]=ans[i-1]+1;
+
+        for(int i=0;i<n-1;i++){
+            if(ratings[i+1]>ratings[i]){
+                ans[i+1]=ans[i]+1;
             }
         }
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                ans[i]=Math.max(ans[i],ans[i+1]+1);
+
+        for(int i=n-1;i>0;i--){
+            if(ratings[i-1]>ratings[i]){
+                ans[i-1]=Math.max(ans[i-1],ans[i]+1);
             }
         }
 
