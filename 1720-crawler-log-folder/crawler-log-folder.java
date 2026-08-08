0a1bac4eaf432bea0c53e0e1748b6b26405f0b87
Sudhir Kumar <sudhir.rit15@gmail.com>
2026-08-08 21:59:54 +0530

    Time: 2 ms (49.10%) | Memory: 44.2 MB (10.18%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> st = new Stack<>();
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            }
+            else if (!log.equals("./")) {
+                st.push(log);
+            }
+        }
+        return st.size();
+    }
+}
