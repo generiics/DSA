9f4cb507d674bac719e15bcd313add38069ca0b8
Sudhir Kumar <sudhir.rit15@gmail.com>
2026-09-16 19:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (85.44%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,18 @@
 class Solution {
+    int[] dp;
     public int fib(int n) {
-        if(n<=1) return n;
-        int[] dp=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
+        dp=new int[n+1];
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            dp[i]=-1;
         }
-        return dp[n];
+        return solve(n);
+    }
+    public int solve(int n){
+         if(n<=1) return n;
+         if(dp[n]!=-1){
+            return dp[n];
+         }
+         dp[n]=solve(n-1)+solve(n-2);
+         return dp[n];
     }
 }
